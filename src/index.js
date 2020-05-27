6f4ec744498e01456e7bb0391ae9225bc9b1ff7f
Luffyfn <faresnasfi@gmail.com>
2020-05-27 11:51:48 +0100

    zdf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import Counter from './Counter';
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+
+const initialState = {
+  count: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+    }
+ };
+
+ const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+ }
+
+const store = createStore(reducer, applyMiddleware(logger));
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Counter />
+    </Provider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
