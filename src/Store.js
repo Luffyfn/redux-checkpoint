3d894c4d86199f500b645f9c2c7960b4356475a5
Luffyfn <faresnasfi@gmail.com>
2020-06-01 09:28:43 +0100

    zdf

--- src/Store.js
+++ src/Store.js
@@ -3,9 +3,7 @@ import {createStore, applyMiddleware} from 'redux';
 import reducer from './Reducer';
 
 const logger = store => next => action => {
-    console.log('dispatching', action)
     let result = next(action)
-    console.log('next state', store.getState())
     return result
    }
   
